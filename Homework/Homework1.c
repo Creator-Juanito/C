8de12b4763a27884edccdc44f0c8cc73a82c6557
Creator-Juanito <78936596+Creator-Juanito@users.noreply.github.com>
2021-02-25 13:53:49 -0600

    Update Homework1.c

--- Homework/Homework1.c
+++ Homework/Homework1.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main(){
+	int c;
+
+    while((c=getchar())!=EOF){
+        if(c=='<'){
+        do{
+           c=getchar();
+        }
+        while(c!=EOF && c!='>');
+    }else{
+        putchar(c);
+        }
+    }
+}
