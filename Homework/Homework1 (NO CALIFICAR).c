8a0bc033a450e575dd0033cd5a50b2fdfb479731
Creator-Juanito <78936596+Creator-Juanito@users.noreply.github.com>
2021-02-25 14:41:26 -0600

    Update Homework1 (NO CALIFICAR).c

--- Homework/Homework1 (NO CALIFICAR).c
+++ Homework/Homework1 (NO CALIFICAR).c
@@ -1,16 +1,1 @@
-#include<stdio.h>
 
-int main(){
-	int c;
-
-    while((c=getchar())!=EOF){
-        if(c=='<'){
-        do{
-           c=getchar();
-        }
-        while(c!=EOF && c!='>');
-    }else{
-        putchar(c);
-        }
-    }
-}
