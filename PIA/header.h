1a0ed888d6175cb034cb3f208c45e9ca8903aca6
Creator-Juanito <78936596+Creator-Juanito@users.noreply.github.com>
2021-05-20 11:52:00 -0500

    Create header.h

---
+++ PIA/header.h
@@ -0,0 +1,23 @@
+/*Nombre de la cabecera: header.h*/
+#include <stdio.h>
+#include <stdlib.h>
+
+double n, num;
+   
+int unoUnidadUno(void);
+int unoUnidadDos(void);
+int unoUnidadTres(void);
+int unoUnidadCuatro(void);
+
+int dosUnidadUno(void);
+int dosUnidadDos(void);
+int dosUnidadTres(void);
+int dosUnidadCuatro(void);
+int dosUnidadCinco(void);
+int dosUnidadSeis(void);
+
+int tresUnidadUno(void);
+int tresUnidadDos(void);
+int tresUnidadTres(void);
+int tresUnidadCuatro(void);
+int tresUnidadCinco(void);
